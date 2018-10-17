5535e776ebb1bb2d756ece831ac1c588893f1df6
Cindy Pallares <cindy.pallaresq@gmail.com>
2018-10-17 01:32:06 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,7 +18,8 @@ def now_serving(deli)
   if deli.size == 0
     puts "There is nobody waiting to be served!"
   else
-    deli.shift
+    name = deli.shift
+    puts "Currently serving #{name}"
   end
   deli
 end
