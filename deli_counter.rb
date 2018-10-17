d4c64dc848309a208da63346240c53d608bb0940
Cindy Pallares <cindy.pallaresq@gmail.com>
2018-10-17 01:23:21 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,8 +2,8 @@ def line(array)
   if array.size == 0
     puts "The line is currently empty."
   else
-    line = "The line is currently: " 
-    array.each_with_index{|person, num| line << "#{num + 1}. #{person}"}
+    line = "The line is currently: "
+    array.each_with_index{|person, num| line << "#{num + 1}. #{person} "}
     puts line
 
   end
